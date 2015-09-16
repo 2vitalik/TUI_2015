a37fe0dfe566fb971081d85b5669517dca6f0b69
lavx64 <slaffon99@gmail.com>
2015-09-16 13:39:49 +0300

    Test

--- Home works/Слава/Test.py
+++ Home works/Слава/Test.py
@@ -1,9 +1,8 @@
 __author__ = 'lavx64'
-#list_1 = []
-#list_1 = raw_input("Enter list\n").split(' ')
-#i = 0
-#while i<len(list_1):
-#    list_1[i]=int(list_1[i])
-#    i+=1
-#print list_1
-
+list_1 = []
+list_1 = raw_input("Enter list\n").split(' ')
+i = 0
+while i<len(list_1):
+    list_1[i]=int(list_1[i])
+    i+=1
+print list_1
