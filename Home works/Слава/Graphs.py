1a47f0a7e078d6aee19a8b6ba2d761ce893759d3
lavx64 <slaffon99@gmail.com>
2015-09-17 10:06:20 +0300

    Make first initialization of graphs

---
+++ Home works/Слава/Graphs.py
@@ -0,0 +1,14 @@
+__author__ = 'lavx64'
+N = {
+    'a': set('bcdef'),
+    'b': set('ce'),
+    'c': set('d'),
+    'd': set('e'),
+    'e': set('f'),
+    'f': set('cgh'),
+    'g': set('fh'),
+    'h': set('fg')
+}
+#TODO
+if 'b' in N['a']:
+    print "Hello"
