5c798dc041155e62c2a1e7e822f9fabb21079654
lavx64 <slaffon99@gmail.com>
2015-09-18 13:32:42 +0300

    Realisation of graphs. Dijkstra path

--- Home works/Слава/Graphs.py
+++ Home works/Слава/Graphs.py
@@ -1,21 +1,63 @@
-__author__ = 'lavx64'
-N = {
-    'a': {'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5},
-    'b': {'c': 2, 'e': 4},
-    'c': {'d': 3},
-    'd': {'e': 4},
-    'e': {'f': 5},
-    'f': {'c': 2, 'g': 6, 'h': 7},
-    'g': {'f': 5, 'h': 7},
-    'h': {'f': 5}
+#!/usr/bin/env python
+
+import networkx as nx
+import matplotlib.pyplot as plot
+
+G = nx.Graph()
+G.add_edge(1, 2, weight=2)
+G.add_edge(1, 6, weight=8)
+G.add_edge(2, 6, weight=9)
+G.add_edge(2, 3, weight=11)
+G.add_edge(2, 7, weight=10)
+G.add_edge(3, 7, weight=5)
+G.add_edge(3, 4, weight=3)
+G.add_edge(4, 7, weight=6)
+G.add_edge(4, 5, weight=12)
+G.add_edge(5, 7, weight=7)
+G.add_edge(5, 6, weight=4)
+G.add_edge(6, 7, weight=13)
+
+pos=nx.spring_layout(G)
+
+nx.draw(G,pos)
+
+edge_labels=dict([((u,v,),d['weight'])
+             for u,v,d in G.edges(data=True)])
+nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+
+print(nx.dijkstra_path(G,1,4))
+
+plot.show()
+
+
+
+
+
+
+
+
+
+
+'''N = {
+    1: {2: 3, 3: 5, 6: 15},
+    2: {4: 4},
+    3: {4: 1},
+    4: {5: 2, 6: 6},
+    5: {6: 1},
+    6: 0
+
 }
-#TODO
-if 'b' in N['a']:
-    print "Hello"
-#smejnost'
 
-print N['a']['b']
-#ves
+ListOfAnswers = []
+l = 1
 
-print len(N['a'])
-#stepen'
+while l != len(N):
+    min = 1000
+    for k in N[l]:
+        if min > N[l][k]:
+            min = k
+    l = min
+    ListOfAnswers.append(min)
+print "End is: ", ListOfAnswers'''
+
+
