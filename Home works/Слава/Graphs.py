229a54df56b6d2ad77ad9875c49e77da0e5b25cf
lavx64 <slaffon99@gmail.com>
2015-09-18 19:34:27 +0300

    Realisation of graphs. Dijkstra path

--- Home works/Слава/Graphs.py
+++ Home works/Слава/Graphs.py
@@ -4,6 +4,23 @@ import networkx as nx
 import matplotlib.pyplot as plot
 
 G = nx.Graph()
+'''
+e=[('a', 'b', 2),
+   ('a','f', 8),
+   ('b','f',9),
+   ('b','c',11),
+   ('b','g',10),
+   ('c','g',5),
+   ('c','d',3),
+   ('d','g',6),
+   ('d','e',12),
+   ('e','g',7),
+   ('e','f',4),
+   ('f','g',13)]
+G.add_weighted_edges_from(e)
+print(nx.dijkstra_path(G, 'a', 'd'))
+'''
+
 G.add_edge(1, 2, weight=2)
 G.add_edge(1, 6, weight=8)
 G.add_edge(2, 6, weight=9)
@@ -17,25 +34,21 @@ G.add_edge(5, 7, weight=7)
 G.add_edge(5, 6, weight=4)
 G.add_edge(6, 7, weight=13)
 
+plot.axis('off')
+
 pos=nx.spring_layout(G)
 
 nx.draw(G,pos)
 
 edge_labels=dict([((u,v,),d['weight'])
              for u,v,d in G.edges(data=True)])
-nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+nx.draw_networkx_edge_labels(G,pos,edge_labels)
 
-print(nx.dijkstra_path(G,1,4))
-
+print "Shortest path from 1 to 4: "
+print(nx.dijkstra_path(G, 1, 4))
+print "Length of this path: "
+print(nx.dijkstra_path_length(G, 1, 4))
 plot.show()
-
-
-
-
-
-
-
-
 
 
 '''N = {
