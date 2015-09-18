1c23fddc227ef5173ab2dae0f6a1deaefe51cbd0
Kostik1 <kostyan4916@gmail.com>
2015-09-18 14:22:58 +0300

    Deikstra

--- Home works/Konstantin_Ya/Deikstra.py
+++ Home works/Konstantin_Ya/Deikstra.py
@@ -1,21 +1,45 @@
 __author__ = 'Kostik'
-a = {
-      1:{2:20,3:30},
-      2:{1:20,3:7,4:5},
-      3:{1:30,2:7,4:10},
-      4:{2:5,3:10,5:93},
-      5:{3:7,4:93}
-    }
-v=[1,2,3,4,5]
-g=[None]*5
-for l in range(0,5):
-     for u,w in a[l].items:
-         if g[l]is None or g[l]>g[l]+w:g[l]+=w
-min = g[0]
-for b in range(0,5,1):
-    if g[b]< min:
-        min = g[b]
-print(min)
+"""a = {
+      0:[(1,10),(2,5)],
+      1:[(0,10),(2,40),(3,50)],
+      2:[(0,5),(1,40),(3,20),(4,25)],
+      3:[(1,50),(2,20),(4,30)],
+      4:[(2,25),(3,30)]
+    }"""
+a = dict()
+with open('graph') as file:
+    w1,w2 = [int(x) for x in file.readline().split()]
+    n,m = [int(x) for x in file.readline().split()]
+    array = []
+    for line in file:
+        u, v, c = line.split()
+        u = int(u)
+        v = int(v)
+        c = int(c)
+        if u in a:
+            a[u].append((v,c))
+        else:
+            a[u] = [(v,c)]
+
+used=[False]*n
+g=[1e9]*n
+g[w1]=0
+for l in range(n):
+    index = -1
+    curres = 1e9
+    for j in range(n):
+        if g[j] < curres:
+            if used[j] == False:
+                index = j
+                curres = g[j]
+    used[index] = True
+    for (u,w) in a[index]:
+        if g[u]>g[index]+w:
+             g[u]=g[index]+w
+
+print("Minimal distance from "+str(w1)+" to "+str(w2)+" is: "+str(g[w2]))
+
+
 
 
 
