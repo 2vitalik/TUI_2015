3897115141f892457021f2de5b0a4103beb2c05c
2vitalik <2vitalik@gmail.com>
2015-10-05 14:10:33 +0300

    add verbose name

--- Main project/db_volonters/storehouse/models.py
+++ Main project/db_volonters/storehouse/models.py
@@ -1,10 +1,11 @@
+# coding: utf-8
 from django.db import models
 
 class StoreHouse(models.Model):
     X = models.IntegerField()
     Y = models.IntegerField()
     V = models.IntegerField()
-    Address = models.CharField(max_length=200)
+    Address = models.CharField(verbose_name=u'Адрес', max_length=200)
 
 
 # Create your models here.
