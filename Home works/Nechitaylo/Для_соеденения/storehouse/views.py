9c2691b001b3af2bf1bf2b0093f2b3611912b6f2
Ivan <korneychuk.ivan.betta.version@gmail.com>
2015-10-09 13:14:54 +0300

    New database

---
+++ Home works/Nechitaylo/Для_соеденения/storehouse/views.py
@@ -0,0 +1,43 @@
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic.base import TemplateView
+from storehouse.models import StoreHouse
+
+
+class MainView(TemplateView):
+    template_name = 'list_storehouse.html'
+
+    def get_context_data(self, **kwargs):
+        context= super(MainView, self).get_context_data(**kwargs)
+        context.update({
+            'StoreHouses':StoreHouse.objects.all()
+        })
+
+
+class StoreHouseListView(ListView):
+    template_name = 'list_storehouse.html'
+    model = StoreHouse
+    context_object_name = 'StoreHouses'
+
+
+class StoreHouseDetailView(DetailView):
+   template_name = 'view_storehouse.html'
+   model = StoreHouse
+   context_object_name = 'StoreHouses'
+
+class StoreHouseCreateView(CreateView):
+    template_name = 'create_storehouse.html'
+    model = StoreHouse
+    context_object_name = 'StoreHouse'
+    fields = ('X', 'Y', 'V', 'Adress')
+    success_url = reverse_lazy('list_StoreHouse')
+
+class StoreHouseUpdateView(UpdateView):
+    template_name = 'update_storehouse.html'
+    model = StoreHouse
+    context_object_name = 'StoreHouse'
+    fields = ('X', 'Y', 'V', 'Adress')
+    success_url = reverse_lazy('list_StoreHouse')
+
+# Create your views here.
