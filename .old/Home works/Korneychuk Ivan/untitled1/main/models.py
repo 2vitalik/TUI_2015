86233505b8e25a842735b8b9915112cd2ed67355
2vitalik <2vitalik@gmail.com>
2015-10-09 13:23:46 +0300

    Ivan's new changes

--- .old/Home works/Korneychuk Ivan/untitled1/main/models.py
+++ .old/Home works/Korneychuk Ivan/untitled1/main/models.py
@@ -1,6 +1,8 @@
 # coding: utf-8
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from django.db import models
-#///////////////////////////////////////////////Volonters///////////////////////////////////////////////////////////////
+
 
 class Volonter(models.Model):
     GENDER_CHOICES = (
@@ -11,17 +13,20 @@ class Volonter(models.Model):
     birthday = models.DateField(null=True, blank=True)
     address = models.TextField()
     telephone = models.CharField(max_length=20)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    conviction = models.BooleanField(default=False)
+    gender = models.CharField(max_length=1,
+                              choices = GENDER_CHOICES)
 
     def __unicode__(self):
         return "%s, %s" % (self.fio, self.gender)
+
+
 class KindOfWork(models.Model):
     name = models.CharField(max_length=100)
-    complexity = models.CharField(max_length=20, null= True)
 
     def __unicode__(self):
-        return self.name
+        return "%s" % (self.name)
+
+
 class Skill(models.Model):
     volonter = models.ForeignKey('Volonter')
     kind = models.ForeignKey('KindOfWork', null=True)
@@ -29,106 +34,25 @@ class Skill(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __unicode__(self):
-        return "%s, %s" % (self.updated_at, self.created_at)
-
-#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class Stock(models.Model):
-    #storeHouseId = models.ForeignKey('Storehouse')
-    #resource = models.ForeignKey('Resource')
-    amount = models.IntegerField(null=True)
-
-    def __unicode__(self):
-        return self.amount
-#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class Supply(models.Model):
-    volonter = models.ForeignKey('Volonter')
-    stock = models.ForeignKey('Stock')
-    dateReal = models.DateField()
-    dateRecomended = models.DateField()
-    amount = models.IntegerField(null=True)
+        return "%s" % (self.kind)
 
 
-    def __unicode__(self):
-        return self.amount
-class Shipping(models.Model):
-    volonter = models.ForeignKey('Volonter')
-    dateRecomended = models.DateField()
-
-    def __unicode__(self):
-        return self.dateRecomended
-class ShippingDetalization(models.Model):
-    amount = models.IntegerField()
-    shipping = models.ForeignKey('Shipping')
-    stock = models.ForeignKey('Stock')
-
-    def __unicode__(self):
-        return self.amount
-#///////////////////////////////////////////transport///////////////////////////////////////////////////////////////////
-class KindOfTransport(models.Model):
+class Resource(models.Model):
+    UNIT_CHOISES = (
+        ('weight',u'Кг'),
+        ('volume',u'Л' ),
+    )
     name = models.CharField(max_length=20)
-    kind = models.CharField(max_length=20)
-    volume = models.CharField(max_length=10)
-    speed = models.CharField(max_length=10)
-    expenseOfFuel = models.CharField(max_length=10)
-    passability = models.CharField(max_length=20)
-    load = models.CharField(max_length=20)
+    unit_of_mesure = models.CharField(max_length = 20,
+                                           choices = UNIT_CHOISES)
+    count = models.FloatField(null=True)
+    def __unicode__(self):
+        return "%s" % (self.name)
+
+
+class Need(models.Model):
+    resource = models.ForeignKey('Resource')
 
     def __unicode__(self):
-        return "%s, %s" % (self.name, self.kind)
-class Transport(models.Model):
-    kindOfTransport = models.ForeignKey('KindOfTransport')
-    number = models.CharField(max_length=10)
-
-    def __unicode__(self):
-        return self.number
-class Employment(models.Model):
-    transport = models.OneToOneField('Transport', null=True)
-    dateStart = models.DateField()
-    dateFinish = models.DateField()
-
-    def __unicode__(self):
-        return "%d, %d" % (self.dateStart, self.dateFinish)
-class Trip(models.Model):
-    transport = models.ForeignKey('Transport')
-    route = models.ForeignKey('Route', null=True)
-    shipping = models.ForeignKey('Shipping')
-    dateDeparture = models.DateField()
-    perfomance = models.BooleanField(default=False)
-#/////////////////////////////////////////////way///////////////////////////////////////////////////////////////////////
-class GeographyPoint(models.Model):
-    x = models.FloatField()
-    y = models.FloatField()
-    address = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.address
-#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class Way(models.Model):
-    gpointFrom = models.ForeignKey('GeographyPoint', related_name='WaygpointFrom')
-    gpointTo = models.ForeignKey('GeographyPoint', related_name='WaygpointTo')
-    s = models.CharField(max_length=15)
-    danger = models.CharField(max_length=20)
-    passability = models.CharField(max_length=20)
-    zagruzhenost = models.CharField(max_length=20)
-#///////////////////////////////////////////////////////route///////////////////////////////////////////////////////////
-class Route(models.Model):
-    #storehouse = models.ForeignKey('Storehouse')
-    #pointOfConsuming = models.ForeignKey('PointOfConsuming')
-    gpointFrom = models.ForeignKey('GeographyPoint', related_name='RoutegpointFrom')
-    gpointTo = models.ForeignKey('GeographyPoint', related_name='RoutegpointTo')
-    name = models.CharField(max_length=30)
-
-    def __unicode__(self):
-        return self.name
-class MakingAWay(models.Model):
-    way = models.ForeignKey('Way')
-    route = models.ForeignKey('Route')
-    sequence=models.IntegerField()
-
-    def __unicode__(self):
-        return self.sequence
-
-
-
-
+        return "%s" % (self.resource)
 
--- .old/Home works/Korneychuk Ivan/untitled1/main/views.py
+++ .old/Home works/Korneychuk Ivan/untitled1/main/views.py
@@ -1,10 +1,11 @@
+from audioop import reverse
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from main.models import Volonter,  KindOfWork, Skill, Transport
-
+from main.models import Volonter, Resource
 
 class MainView(TemplateView):
     template_name = 'list_volonter.html'
@@ -12,74 +13,47 @@ class MainView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MainView, self).get_context_data(**kwargs)
         context.update({
+            'hello': 'world',
             'volonters': Volonter.objects.all(),
         })
         return context
+
+
 class VolonterListView(ListView):
     template_name = 'list_volonter.html'
     model = Volonter
     context_object_name = 'volonters'
+
 class VolonterDetailView(DetailView):
     template_name = 'view_volonter.html'
     model = Volonter
     context_object_name = 'volonter'
+
+class VolonterCreateView(CreateView):
+    template_name = 'create_volonter.html'
+    model = Volonter
+    fields = ('fio', 'address',
+              'telephone', 'gender')
+    success_url = reverse_lazy('list_volonters')
+
+class VolonterUpdateView(UpdateView):
+    template_name = 'update_volonter.html'
+    model = Volonter
+    context_object_name = 'volonter'
+    fields = ('fio', 'address',
+              'telephone', 'gender')
+
 #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class KindOfWorkMainView(TemplateView):
-    template_name = 'list_kindofwork.html'
+class ResourceMainView(TemplateView):
+    template_name = 'list_resource.html'
+
     def get_context_data(self, **kwargs):
-        context = super(KindOfWorkMainView, self).get_context_data()
-        context.update({'kWorks':KindOfWork.objects.all()})
+        context = super(ResourceMainView, self).get_context_data(**kwargs)
+        context.update({
+            'Resources': Resource.objects.all(),
+        })
         return context
-class KindOfWorkListView(ListView):
-    template_name = 'list_kindofwork.html'
-    model = KindOfWork
-    context_object_name = 'kWork'
-class KindOfWorkDetailView(DetailView):
-    template_name = 'view_kindofwork.html'
-    model = KindOfWork
-    context_object_name = 'kWork'
-#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class SkillMainView(TemplateView):
-    template_name = 'list_skill.html'
-    def get_context_data(self, **kwargs):
-        context = super(SkillMainView, self).get_context_data()
-        context.update({'skills': Skill.objects.all()})
-        return context
-class SkillListView(ListView):
-    template_name = 'list_skill.html'
-    model = Skill
-    context_object_name = 'skills'
-class SkillDetailView(DetailView):
-    template_name = 'view_skill.html'
-    model = Skill
-    context_object_name = 'skill'
-#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class TransportMainView(TemplateView):
-    template_name = 'list_transport.html'
-    def get_context_data(self, **kwargs):
-        context = super(TransportMainView, self).get_context_data()
-        context.update({'trans': Transport.objects.all()})
-        return context
-class TransportListView(ListView):
-    template_name = 'list_transport.html'
-    model = Transport
-    context_object_name = 'trans'
-class TransportDetailView(DetailView):
-    template_name = 'view_transport.html'
-    model = Transport
-    context_object_name = 'trans'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class ResourceListView(ListView):
+    template_name = 'list_resource.html'
+    model = Resource
+    context_object_name = 'Resources'
