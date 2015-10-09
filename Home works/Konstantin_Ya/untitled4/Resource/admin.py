8acdf2277df4600056f45bd3d7fa09092d5b33eb
Kostik1 <kostyan4916@gmail.com>
2015-10-09 12:31:39 +0300

    fff

--- Home works/Konstantin_Ya/untitled4/Resource/admin.py
+++ Home works/Konstantin_Ya/untitled4/Resource/admin.py
@@ -1,7 +1,9 @@
+from sqlite3 import adapt
+from django.conf.app_template import admin
 from django.contrib import admin
 
 # Register your models here
-from Resource.models import Resource, Need
+from Resource.models import Resource, Need, PointOfConsuming, Order
 
 
 class ResourceAdmin(admin.ModelAdmin):
@@ -21,10 +23,23 @@ class ResourceAdmin(admin.ModelAdmin):
 
 class NeedAdmin(admin.ModelAdmin):
     list_display = (
+        'id_resource',
         'number_of_resource',
         'priority',
         'perfomance',
     )
-
+class PointOfConsumingAdmin(admin.ModelAdmin):
+    list_display = (
+        'fio',
+        'telephone',
+    )
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'date_of_starting',
+        'date_of_finish',
+        'priority',
+    )
 admin.site.register(Resource, ResourceAdmin)
 admin.site.register(Need, NeedAdmin)
+admin.site.register(PointOfConsuming,PointOfConsumingAdmin)
+admin.site.register(Order,OrderAdmin)
--- Home works/Konstantin_Ya/untitled4/Resource/models.py
+++ Home works/Konstantin_Ya/untitled4/Resource/models.py
@@ -5,19 +5,20 @@ from django.db import models
 # Create your models here.
 class Resource(models.Model):
     name = models.CharField(max_length=20)
-    weight = models.FloatField(null = True)
-    volume = models.FloatField(null = True)                                        
-    count = models.FloatField(null = True)
+    weight = models.FloatField()
+    volume = models.FloatField()
+    count = models.FloatField(0)
     def __unicode__(self):
         return "%s, %f, %f, %f" % (self.name, self.weight, self.volume, self.count)
 
 class Need(models.Model):
-    number_of_resource = models.ForeignKey('Resource', null = True)
+    id_resource = models.ForeignKey('Resource', null = True)
+    number_of_resource = models.IntegerField(null = True)
     priority = models.FloatField(null = True)
     perfomance = models.IntegerField(null = True)
     id_order = models.ForeignKey('Order', null = True)
     def __unicode__(self):
-        return "%f, %i" % (self.priority, self.perfomance)
+        return "%i, %f, %i" % (self.number_of_resource, self.priority, self.perfomance)
 
 class Order(models.Model):
     #id_geography_point = models.ForeignKey(GeographyPoint)
@@ -25,5 +26,11 @@ class Order(models.Model):
     date_of_finish = models.DateField(null = False)
     priority = models.FloatField(null = True)
     def __unicode__(self):
-        return "%d, %d, %f" % (self.date_of_starting, self.date_of_finish, self.priority)
-
+        return "%s, %s, %f" % (self.date_of_starting, self.date_of_finish, self.priority)
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$/storehouse/$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+class PointOfConsuming(models.Model):
+    #id_geography_point = models.ForeignKey(GeographyPoint)
+    fio = models.CharField(max_length=50, null = False)
+    telephone = models.CharField(max_length=11, null = True)
+    def __unicode__(self):
+        return "%s, %s" % (self.fio, self.telephone)
