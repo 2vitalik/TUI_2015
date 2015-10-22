9b8ce2131ec364a42829bdaf1b73755696cf1ba0
FunnyEnd <kornienko_aa@yahoo.com>
2015-10-22 09:41:50 +0300

    fix, remove and comment

--- storehouse/views.py
+++ storehouse/views.py
@@ -1,10 +1,10 @@
-from django.core.urlresolvers import reverse_lazy
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from django.views.generic.base import TemplateView
-from storehouse.models import StoreHouse
-
-
+# from django.core.urlresolvers import reverse_lazy
+# from django.shortcuts import render
+# from django.views.generic import ListView, DetailView, CreateView, UpdateView
+# from django.views.generic.base import TemplateView
+# from storehouse.models import StoreHouse
+#
+#
 # class MainView(TemplateView):
 #     template_name = 'list_storehouse.html'
 #
