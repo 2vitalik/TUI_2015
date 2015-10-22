be0e2ce29955965c2e436433a2998396e1752225
Kostik1 <kostyan4916@gmail.com>
2015-10-22 13:22:29 +0300

    add new migrations

---
+++ main/migrations/0017_auto_20151022_1321.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0016_order'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='storehouse',
+            name='free_volume',
+            field=models.FloatField(default=models.IntegerField()),
+        ),
+        migrations.AlterField(
+            model_name='stock',
+            name='storeHouseId',
+            field=models.ForeignKey(to='main.StoreHouse', null=True),
+        ),
+        migrations.AlterField(
+            model_name='storehouse',
+            name='address',
+            field=models.CharField(max_length=100, verbose_name=b'geography_point.address'),
+        ),
+    ]
