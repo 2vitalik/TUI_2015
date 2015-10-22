bf332de946b4e7f06403ee1a56416560f1d08364
Ivan <korneychuk.ivan.betta.version@gmail.com>
2015-10-22 13:14:52 +0300

    add migrations

---
+++ main/migrations/0016_order.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0015_resourceorder'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('needs', models.ManyToManyField(to='main.Order')),
+            ],
+        ),
+    ]
