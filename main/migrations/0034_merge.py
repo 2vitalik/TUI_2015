951d9b18c7532474dc24fe0f8b874f87a20a846e
Kostik1 <kostyan4916@gmail.com>
2015-10-28 17:06:37 +0200

    merge migration

---
+++ main/migrations/0034_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0033_auto_20151028_1623'),
+        ('main', '0028_auto_20151028_1605'),
+    ]
+
+    operations = [
+    ]
