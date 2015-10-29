07a56e861c2c39af81eebcaf64c13dee2a5e6d26
Kostik1 <kostyan4916@gmail.com>
2015-10-29 14:49:15 +0200

    merge

---
+++ main/migrations/0041_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0040_auto_20151029_0127'),
+        ('main', '0036_order_name'),
+    ]
+
+    operations = [
+    ]
