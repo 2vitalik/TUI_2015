4cdbf9e609855e4efa0720de1165b58542b8eaa9
Vitalik <2vitalik@gmail.com>
2015-11-01 01:40:43 +0200

    ссылочка на запуск general_algo() из админки

--- db_volonters/urls.py
+++ db_volonters/urls.py
@@ -19,7 +19,8 @@ from django.contrib.auth.views import login, logout
 from main.views import MainView, VolonterListView, VolonterDetailView, VolonterCreateView, VolonterGrafikView, \
     CreateVolontersView, ResourceGrafikView, CreateNeedsView, CreatePointOfConsumingView,FinishedView, \
     ResourceListView, DeleteCandidateVolonterView, ActivateCandidateVolonterView,MoneyView, GraphView, \
-    CreateOrderView, NeedListView, NeedCreateView, MapView, AboutView, AviceView, RoatView, LeliksView, CreateRoat
+    CreateOrderView, NeedListView, NeedCreateView, MapView, AboutView, AviceView, RoatView, LeliksView, CreateRoat, \
+    GeneralAlgoView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -95,6 +96,8 @@ urlpatterns = [
     # url(r'^PointOfConsuming/add/$',
     #     PointOfConsumingCreateView.as_view(),
     #     name = 'create_pointofconsuming'),
+
+    url(r'^general_algo/$', GeneralAlgoView.as_view(), name='general_algo'),
 
     url(r'^actions/finished/(?P<resource_order_id>\d+)/',
         FinishedView.as_view(),
