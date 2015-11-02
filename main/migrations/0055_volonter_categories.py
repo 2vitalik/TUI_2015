cfd07a0ce0da04d85dd2be8c4a3fefe791ef18d6
Kostik1 <kostyan4916@gmail.com>
2015-11-02 11:35:45 +0200

    fix migration

--- main/migrations/0055_volonter_categories.py
+++ main/migrations/0055_volonter_categories.py
@@ -11,9 +11,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='volonter',
-            name='categories',
-            field=models.ManyToManyField(to='main.CategoryResource', verbose_name='\u041a\u0430\u0442\u0435\u0433\u043e\u0440\u0456\u044f \u0440\u0435\u0441\u0443\u0440\u0441\u0456\u0432'),
-        ),
+        # migrations.AddField(
+        #     model_name='volonter',
+        #     name='categories',
+        #     field=models.ManyToManyField(to='main.CategoryResource', verbose_name='\u041a\u0430\u0442\u0435\u0433\u043e\u0440\u0456\u044f \u0440\u0435\u0441\u0443\u0440\u0441\u0456\u0432'),
+        # ),
     ]
